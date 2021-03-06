814c87ab5977883d4855df746b305751c04d476d
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-06 12:36:25 +0800

    add tests and refactored code

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,4 +1,4 @@
-require_relative './gilded_rose'
+require_relative './gilded_rose_v2'
 
 RSpec.describe "GildedRose" do
   let!(:items) do
@@ -10,5 +10,96 @@ RSpec.describe "GildedRose" do
      Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
      Item.new("Conjured Mana Cake", 3, 6),
     ]
+  end
+
+  context "Normal items" do
+    it "Should decrease sell_in by 1 after 1 day" do
+      update_quality(items)
+      expect(items[0].sell_in).to eq 9
+      expect(items[2].sell_in).to eq 4
+    end
+    it "Should decrease in quality by 1 after 1 day" do
+      update_quality(items)
+      expect(items[0].quality).to eq 19
+      expect(items[2].quality).to eq 6
+    end
+    it "Should decrease in quality twice as fast after sell_in days" do
+      item = [Item.new("Broadsword", 1, 50)]
+      for i in 1..5
+        update_quality(item)
+      end
+      expect(item[0].quality).to eq 41
+    end
+    it "Should never have a negative quality" do
+      for i in 1..100
+        update_quality(items)
+      end
+      expect(items[0].quality).to eq 0
+      expect(items[2].quality).to eq 0
+    end
+  end
+
+  context "Sulfuras, Hand of Ragnaros" do
+    it "Should not decrease in quality or sell_in" do
+      for i in 1..10
+        update_quality(items)
+      end
+      expect(items[3].quality).to eq 80
+      expect(items[3].sell_in).to eq 0
+    end
+  end
+
+  context "Aged Brie" do
+    it "Should increase in quality by 1 after 1 day" do
+      update_quality(items)
+      expect(items[1].quality).to eq 1
+    end
+    it "Should increase in quality by 2 after expiration day" do
+      brie = [Item.new("Aged Brie", 0, 3)]
+      update_quality(brie)
+      expect(brie[0].quality).to eq 5
+    end
+    it "Should never exceed quality of 50" do
+      brie = [Item.new("Aged Brie", 0, 3)]
+      for i in 1..100
+        update_quality(brie)
+      end
+      expect(brie[0].quality).to eq 50
+    end
+  end
+
+  context "Backstage passes" do
+    it "Should increase quality by 2 if there are 10 days or less before event" do
+      for i in 1..6
+        update_quality(items)
+      end
+      expect(items[4].quality).to eq 27
+    end
+    it "Should increase quality by 3 if there are 5 days or less before event" do
+      for i in 1..11
+        update_quality(items)
+      end
+      expect(items[4].quality).to eq 38
+    end
+    it "Should drop quality to 0 if event has passsed" do
+      for i in 1..16
+        update_quality(items)
+      end
+      expect(items[4].quality).to eq 0
+    end
+    it "Should never exceed quality of 50" do
+      item = [Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 45)]
+      for i in 1..15
+        update_quality(item)
+      end
+      expect(item[0].quality).to eq 50
+    end
+  end
+
+  context "Conjured items" do
+    it "Should decrease in quality twice as fast as normal items i.e. decrease by 2 every day" do
+      update_quality(items)
+      expect(items[5].quality).to eq 4
+    end
   end
 end
---
+++ gilded_rose_v2.rb
@@ -0,0 +1,61 @@
+def update_quality(items)
+  items.each do |item|
+    adjust_quality(item)
+    adjust_sell_in(item)
+  end
+end
+
+def adjust_sell_in(item)
+  if !is_legendary(item)
+    item.sell_in -= 1
+  end
+end
+
+def adjust_quality(item)
+  if is_brie(item)
+    item.quality += 1
+    if item.sell_in <= 0
+      item.quality += 1
+    end
+  elsif is_backstage_pass(item)
+    case item.sell_in
+    when 1..5
+      item.quality += 3
+    when 6..10
+      item.quality += 2
+    when 11..nil
+      item.quality += 1
+    when 0
+      item.quality = 0
+    end
+  elsif is_legendary(item)
+  else #for normal items and conjured items
+    is_conjured(item) ? n = 2 : n = 1
+    for i in 1..n
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in <= 0
+    end
+  end
+  item.quality = 50 if (item.quality > 50) && !is_legendary(item)
+  item.quality = 0 if item.quality < 0
+end
+
+def is_brie(item)
+  true if (item.name.include? "Aged Brie")
+end
+
+def is_backstage_pass(item)
+  true if (item.name.include? "Backstage")
+end
+
+def is_conjured(item)
+  true if (item.name.include? "Conjured")
+end
+
+def is_legendary(item)
+  true if (item.name.include? "Sulfuras")
+end
+
+# DO NOT CHANGE THINGS BELOW -----------------------------------------
+
+Item = Struct.new(:name, :sell_in, :quality)
